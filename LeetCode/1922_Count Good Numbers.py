8bbe9a38429310a1a2047ee8334dc6ba7ff10f57
季憲 蔡 <jih-shiann@hotmail.com>
2025-04-13 15:47:51 +0800

    LeetCode 1922

---
+++ LeetCode/1922_Count Good Numbers.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def countGoodNumbers(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        
+        # 0 1 2 3 4 5
+        # °¸©_°¸©_°¸©_...
+
+        even = (n+1) // 2
+        odd = n // 2
+        # ans = (5**even) * (4**odd) % MOD
+
+        # pow(base, exp, mod)  ¡Ý  (base**exp) % mod
+        # (a modm)¡Ñ(b modm) mod m= (a¡Ñb) mod m,
+        ans = pow(5, even, MOD) * pow(4, odd, MOD) % MOD
+
+
+
+        
+        return ans
+
+        
